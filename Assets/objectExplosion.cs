18d55543be9459ce24315558dd97aac3d164fe37
Romeo Gatcheco III <romeogatcheco1207@gmail.com>
2018-03-09 10:13:08 +0800

    Add Health System

--- Assets/objectExplosion.cs
+++ Assets/objectExplosion.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class objectExplosion : MonoBehaviour {
     public ParticleSystem enemyExplosion;
+    public ParticleSystem enemyDamaged;
     public bool _sabog;
+    public int _EnemyHealth;
+    public bool _isDamaged;
     // Use this for initialization
     void Start () {
         _sabog = false;
+        _isDamaged = false;
+        _EnemyHealth = 100;
 	}
 	
 	// Update is called once per frame
@@ -16,10 +21,21 @@ public class objectExplosion : MonoBehaviour {
 	}
     void OnParticleCollision(GameObject other)
     {
-        if (_sabog == false)
+        if (!_sabog)
         {
-            if (other.gameObject.tag == "bullet")
+            if (other.gameObject.tag == "bullet" && _EnemyHealth > 0)
             {
+                _EnemyHealth--;
+                print(_EnemyHealth);
+            }
+            if (_EnemyHealth <= 50 && !_isDamaged)
+            {
+                enemyDamaged.Play();
+                _isDamaged = true;
+            }
+            if(_EnemyHealth <= 0 && !_sabog)
+            {
+                Destroy(gameObject, 1f);
                 enemyExplosion.Play();
                 _sabog = true;
             }
