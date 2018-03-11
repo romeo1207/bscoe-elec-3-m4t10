13429690d81445fee75cff8e443705ce9320dc9a
Romeo Gatcheco III <romeogatcheco1207@gmail.com>
2018-03-12 00:12:09 +0800

    Add Scoring System

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    public static int Score;
+
+    public Text ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+    void Awake(){
+        ScoreText = GetComponent<Text>();
+
+        Score = 0;
+    }	
+	// Update is called once per frame
+	void Update () {
+        ScoreText.text = "Score: " + Score;
+	}
+}
--- Assets/objectExplosion.cs
+++ Assets/objectExplosion.cs
@@ -8,16 +8,18 @@ public class objectExplosion : MonoBehaviour {
     public bool _sabog;
     public int _EnemyHealth;
     public bool _isDamaged;
+    AudioSource _audioSource;
     // Use this for initialization
     void Start () {
         _sabog = false;
         _isDamaged = false;
-        _EnemyHealth = 100;
-	}
+        _EnemyHealth = 10;
+        _audioSource = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
     void OnParticleCollision(GameObject other)
     {
@@ -28,16 +30,19 @@ public class objectExplosion : MonoBehaviour {
                 _EnemyHealth--;
                 print(_EnemyHealth);
             }
-            if (_EnemyHealth <= 50 && !_isDamaged)
+            if (_EnemyHealth <= 6 && !_isDamaged)
             {
                 enemyDamaged.Play();
                 _isDamaged = true;
             }
             if(_EnemyHealth <= 0 && !_sabog)
             {
+                gameObject.tag = "Untagged";
                 Destroy(gameObject, 1f);
                 enemyExplosion.Play();
                 _sabog = true;
+                ScoreManager.Score += 10;
+                _audioSource.Play();
             }
         }
     }
